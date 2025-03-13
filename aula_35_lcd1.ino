ce5b6f649a3a066ab89ef1fbe7e1d7aaf0b9a39c
LP <email@riquelme.delmondes@aluno.senai.br>
2025-03-13 16:20:28 -0300

    aula 35 lcd

---
+++ aula_35_lcd1.ino
@@ -0,0 +1,30 @@
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+#define Botao 8
+
+int estadoAnterior = 0;
+int estadoAtual = 1;
+
+
+void setup()
+{
+ lcd.init();
+ lcd.backlight();
+ pinMode(Botao, INPUT);
+}
+void loop()
+{
+  int estadoBotao = digitalRead(Botao);
+  if(estadoBotao == 1 && estadoAnterior == 0){
+  lcd.print(estadoAtual);
+    estadoAtual++;
+  if (estadoAtual == 10)
+   estadoAtual = 0;
+
+  
+  }
+  estadoAnterior = estadoBotao;
+}
+ 
